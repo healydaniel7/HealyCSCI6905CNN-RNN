926e4db33d58f73a6c79c5e5b0aac14199e07359
Daniel Healy <healy108@mail.chapman.edu>
2018-06-18 15:22:48 -0700

    added stats to cnn

--- cnn.py
+++ cnn.py
@@ -6,6 +6,8 @@ from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
 from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
 from keras.optimizers import RMSprop
+import tensorflow as tf
+import sklearn.metrics as metrics
 
 def get_image_size():
     first_image = load_img('data/train/noPneumonia/IM-0001-0001.jpg')
@@ -18,6 +20,20 @@ def create_generator(generator, directory, size, batch):
         target_size=size,
         batch_size=batch,
         class_mode='binary')
+
+def as_keras_metric(method):
+    import functools
+    from keras import backend as K
+    import tensorflow as tf
+    @functools.wraps(method)
+    def wrapper(self, args, **kwargs):
+        """ Wrapper for turning tensorflow metrics into keras metrics """
+        value, update_op = method(self, args, **kwargs)
+        K.get_session().run(tf.local_variables_initializer())
+        with tf.control_dependencies([update_op]):
+            value = tf.identity(value)
+        return value
+    return wrapper
 
 def create_sequential_model(image_size):
     model = Sequential()
@@ -47,8 +63,8 @@ zoom_rge = 0.15
 learn_rate = .0001
 aug_batch_size = 16
 test_batch_size = 32
-train_vals = 100
-test_vals = 40
+train_vals = 50
+test_vals = 20
 epochs = 15
 if __name__ == '__main__':
     image_size = get_image_size()
@@ -58,13 +74,27 @@ if __name__ == '__main__':
     v_gen = create_generator(image_gen, 'data/validation/', (image_size[0], image_size[1]), test_batch_size)
     seq_model = create_sequential_model(image_size)
     optimizer_prop = RMSprop(lr=learn_rate)
+    precision = as_keras_metric(tf.metrics.precision)
+    recall = as_keras_metric(tf.metrics.recall)
     seq_model.compile(loss='binary_crossentropy',
               optimizer=optimizer_prop,
-              metrics=['accuracy'])
+              metrics=['accuracy', recall, precision])
     seq_model.fit_generator(
         t_gen,
         steps_per_epoch=train_vals/aug_batch_size,
         nb_epoch=epochs,
         validation_data=v_gen,
         validation_steps=test_vals)
+
+    pred = seq_model.predict_generator(v_gen)
+    predicted_classes = np.argmax(pred, axis=1)
+
+    true_classes = v_gen.classes
+    class_labels = list(v_gen.class_indices.keys())
+
+    report = metrics.classification_report(true_classes, predicted_classes, target_names=class_labels)   
+
     print("CNN Model Accuracy: " + str(seq_model.evaluate_generator(v_gen, test_vals)[1] * 100) + "%")
+    print("CNN Model Recall: " + str(seq_model.evaluate_generator(v_gen, test_vals)[2] * 100) + "%")
+    print("CNN Model Precision: " + str(seq_model.evaluate_generator(v_gen, test_vals)[3] * 100) + "%")
+    print("CNN Model Confusion: " + str(report))
